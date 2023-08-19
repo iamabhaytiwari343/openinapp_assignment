3dde9fbf33cc8b8dcf5978982ea03b606b82c6d7
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-08-20 01:41:53 +0530

    grid made

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-        
+      <div class="column black-column">
+        <h1 class="centered-text">Boards.</h1>
+      </div>
+      <div class="column white-column">
+
+
+      </div>
     </div>
   );
 }
