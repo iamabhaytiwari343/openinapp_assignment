be0997acadaa09f342e08ce513c21b9e98d12ae8
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-10-07 22:54:42 +0530

    dashboard creating

--- src/Components/Dashboard/Board.jsx
+++ src/Components/Dashboard/Board.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Sidebar from './Sidebar';
-import "./css/board.css"
-// import Main from './Main';
+import "./css/board.css";
 const Board = () => {
   return (
     <>
@@ -9,11 +8,21 @@ const Board = () => {
         <div className="sidenav">
           <Sidebar />
         </div>
-        <div className="main">
-          {/* <Main className="navigationmenu" /> */}
+        <div className="maincontainerdashoard">
+          
+          <div class="horizontal-navbar">
+            <p>Dashboard</p>
+            <div class="search-bar">
+              <i class="fas fa-search"></i>
+              <input type="text" placeholder="Search" />
+            </div>
+            <i class="fas fa-bell"></i>
+            <div class="profile-circle">
+            </div>
+          </div>
+
         </div>
       </div>
-
     </>
   )
 }
