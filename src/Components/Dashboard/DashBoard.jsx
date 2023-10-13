26fb56237bb2e88aec1cf4b2afd5f436eec0c676
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-10-13 23:36:19 +0530

    created cards responsive and sidebar responsive

--- src/Components/Dashboard/DashBoard.jsx
+++ src/Components/Dashboard/DashBoard.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import "./customStylesheet.css";
+import Sidebar from './Sidebar';
+import icon from "./utils/icon.png";
+import Card from './utils/Cards';
 const DashBoard = () => {
     return (
         <>
             <div className="dashboardContainer">
                 <div className="sidebarContainer">
-                    Sidebar
+                    <Sidebar />
                 </div>
                 <div className="mainContentContainer">
                     <div className="navbarContainer">
                         NAVBAR
                     </div>
                     <div className="statsCardContainer">
-                        Stats
+                        <div className="cards">
+                            <Card icon={icon} vari="Total Revenue" totalRevenue="2129430" percentageChange="2.5" />
+                            <Card icon={icon} vari="Total Transaction" totalRevenue="1520" percentageChange="1.7" />
+                            <Card icon={icon} vari="Total Likes" totalRevenue="9721" percentageChange="1.4" />
+                            <Card icon={icon} vari="Total Users" totalRevenue="9721" percentageChange="4.2" />
+                        </div>
                     </div>
                     <div className="barChartContainer">
                         barChartContainer
